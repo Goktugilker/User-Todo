5e7ae5f78b2e283e2dcd863ffa4faccab60075db
Göktuğ İlker <240928036@ogr.comu.edu.tr>
2025-07-17 15:01:53 +0300

    he kullanıcıya ayrı sayfa olacak şekilde ayarlandı

---
+++ app/components/Todo/todoSave.vue
@@ -0,0 +1,7 @@
+<template>
+  <UButton
+    label="Todo Ekle"
+    icon="lucide:user-round-search"
+    class="bg-success-600 text-white"
+  />
+</template>
---
+++ app/pages/todo/[id].vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const userStore = useApiStore()
+</script>
+
+<template>
+    <div
+        class="w-dvw h-dvh flex flex-row items-center justify-center gap-4 scale-100 overflow-hidden"
+        :class="isDark() ? 'bg-zinc-600 text-white' : 'bg-gray-800 text-white' "
+    >
+    <h1>{{ userStore.user }}</h1>
+        <div
+        class="flex flex-row gap-4 text-white w-dvw h-dvh justify-center items-center mb-96 "
+        >
+        <TodoSave />
+        <TodoUpload/>
+        </div>
+    </div>
+</template>
