66121b2307ea9e6f370b288fcd2677e999322719
Göktuğ İlker <240928036@ogr.comu.edu.tr>
2025-07-22 10:24:21 +0300

    Refactor imports and remove unused dependencies; update type declarations and clean up index.vue layout

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -14,13 +14,13 @@ onMounted(async () => {
 <template>
   <div class="w-screen flex flex-column h-dvh overflow-hidden">
     <div
-    class="w-dvw h-dvh flex flex-row items-center justify-center gap-4 scale-110 "
-    :class="isDark ? 'bg-zinc-600 text-white' : 'bg-gray-800 text-white' "
-  >
-    <div
-      class="flex flex-row gap-4 text-white w-dvw h-dvh justify-center items-center "
+      class="w-dvw h-dvh flex flex-row items-center justify-center gap-4 scale-110 "
+      :class="isDark ? 'bg-zinc-600 text-white' : 'bg-gray-800 text-white' "
     >
-      <!-- <TodoUpload />
+      <div
+        class="flex flex-row gap-4 text-white w-dvw h-dvh justify-center items-center "
+      >
+        <!-- <TodoUpload />
       <TodoUserReload />
       <USelectMenu
         v-model="userStore.user"
@@ -29,10 +29,10 @@ onMounted(async () => {
         class="w-48"
         :class="isDark ? 'border-zinc-800 shadow-lg shadow-amber-600 bg-zinc-700 text-white' : 'border-gray-700 shadow-lg shadow-gray-600 bg-gray-700 text-white' "
       /> -->
-      <home-navigation-menu class="scale-150 " />
+        <home-navigation-menu class="scale-150 " />
+      </div>
+      <div />
     </div>
-    <div />
-  </div>
   </div>
 </template>
 
